ba0b81e50ee45f3ed50897a3061248dba0637b1d
Nick <Nickvuletich23@gmail.com>
2026-06-05 17:47:06 -0400

    fixing python

--- src/embed.py
+++ src/embed.py
@@ -21,3 +21,27 @@ print number of embeddings
 print embedding dimension
 
 """
+from sentence_transformers import SentenceTransformer
+from ingest import load_documents
+
+def load_embedding_model(model_name="all-MiniLM-L6-v2"):
+    model = SentenceTransformer(model_name)
+    return model
+
+def embed_texts(texts, model):
+    embeddings = model.encode(texts)
+    embeddings = embeddings.to_list()
+    return embeddings
+
+if __name__ == "__main__":
+    model = load_embedding_model()
+
+    texts = [
+        "protein was 185 grams today",
+        "bench press felt strong"
+    ]
+
+    embeddings = embed_texts(texts, model)
+
+    print(len(embeddings))
+    print(len(embeddings[0]))
